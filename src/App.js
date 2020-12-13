e579e0313d926b0c5ea76e9456a5626f809ad1d5
Roham Hussain <roham.hussain@gmail.com>
2020-12-13 17:42:29 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 // Import the BrowserRouter, Route and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js';
 import Articles from './Articles.js';
 import About from './About.js';
 import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
@@ -11,9 +12,25 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+  
+        {/* Set up the Router */}
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          {/* <img src={logo} className="logo-image" alt="Logo Image" /> */}
+          <div className="navigation-sub">
+
+            {/* Set up the Links */}
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+
+          </div>
+        </div>
 
         <div className="projects">
-          <h1>test</h1>
+          <h1>Projects</h1>
           <Thumbnail
             link="/twitter"
             image="http://twitter-image-url.jpg"
@@ -35,6 +52,7 @@ function App() {
             category="Desktop App"
           />
         </div>
+
 
       </div>
     </BrowserRouter>
