d339ad7d5239d373da01d4c3562238a6782c080c
Roham Hussain <roham.hussain@gmail.com>
2025-03-15 14:42:40 -0500

    refining about me section + started sidescroll gallery

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,6 +6,16 @@ import '../styles/App.scss';
  
 function Grid(props) {
   //change from "test_projects" to "projects" soon
+  const test_grid = data.test_projects.map(project => (
+    <Thumbnail
+      key = {project.id}
+      link = {project.link}
+      image = {project.image}
+      title = {project.title}
+      tagline = {project.tagline}
+    />
+  ));
+
   const grid = data.projects.map(project => (
     <Thumbnail
       key = {project.id}
@@ -25,7 +35,7 @@ function Grid(props) {
 
       <div className="about">
         <div className="about_text">
-          <h1>Hello, my name is Roham.</h1>
+          <h1>Hello! My name is Roham.</h1>
           <p>I love creating meaningful, long-lasting, and usable experiences. I am an <strong>experience-builder</strong> across physical and digital media.</p>
         </div>
       </div>
@@ -36,8 +46,8 @@ function Grid(props) {
       </div>
 
       <div className="gallery_grid">
-        <h1>Gallery</h1>
-        {/*<ul className="flex_projects">{grid}</ul>*/}
+        <h2>Gallery</h2>
+        <ul className="flex_gallery">{test_grid}</ul>
       </div>
 
 
