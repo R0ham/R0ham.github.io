36fc3c5fb4beb5e76df99f0b3e290bb7d6814eee
Roham Hussain <roham.hussain@gmail.com>
2025-04-13 20:30:06 -0500

    updated bio

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -36,7 +36,7 @@ function Grid(props) {
       <div className="about">
         <div className="about_text">
           <h1>Hello! My name is Roham.</h1>
-          <p>I am a creative thinker, with strong visual and technical skills. In my free time, I like to learn and experience new activities! I recently wrote a short story, enjoyed paddle boarding for the first time, and continue to be an avid biker. I love well-told stories and will consume them in all forms. I like challenging myself to continuously grow and will frequently self-reflect to do so. I’m also passionate and driven, and will proactively find opportunity areas I want to participate in. I thrive most in freeform environments with close-knit teams.</p>
+          <p>I am a creative thinker, with strong visual and technical skills. I recently wrote a short story, enjoyed paddle boarding for the first time, and continue to be an avid biker. I love learning new things and by doing so, I challenge myself to continuously grow. I’m also passionate and driven, and will proactively find opportunity areas I want to participate in. I thrive most in freeform environments within a closely working team.</p>
         </div>
       </div>
 
